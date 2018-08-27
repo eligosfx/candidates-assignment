84ed808e947f00feeacaf0185a58fbab74798266
tlv\arkadyl <arkadyl@liveperson.com>
2018-08-28 00:38:15 +0300

    exercise3

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+ssh-keygen
+ssh-copy-id -i ~/.ssh/id_rsa.pub server2
+
+cat <<EOT >> /home/vagrant/.ssh/config
+Host *
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+EOT
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+ssh-keygen
+ssh-copy-id -i ~/.ssh/id_rsa.pub server1
+
+cat <<EOT >> /home/vagrant/.ssh/config
+Host *
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+EOT
